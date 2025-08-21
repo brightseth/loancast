acefebb0242f400a966847f5faba4e104e8d7aac
brightseth <165308529+brightseth@users.noreply.github.com>
2025-08-21 14:50:07 -0700

    fix: Completely remove ALL frame metadata from blog
    - Set fc:frame to empty string to override vNext
    - Remove all Open Frames metadata too
    - Blog posts should NOT be frames at all
    - This MUST fix the Frames v1 error

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -3,13 +3,20 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   // Override the frame metadata from root layout - blog posts are not frames
   other: {
-    // Remove frame buttons to make this NOT a frame
+    // Completely disable frames for blog posts
+    'fc:frame': '',
+    'fc:frame:image': '',
+    'fc:frame:image:aspect_ratio': '',
     'fc:frame:button:1': '',
     'fc:frame:button:1:action': '',
     'fc:frame:button:1:target': '',
     'fc:frame:button:2': '',
     'fc:frame:button:2:action': '', 
     'fc:frame:button:2:target': '',
+    'of:version': '',
+    'of:accepts:farcaster': '',
+    'of:image': '',
+    'of:image:aspect_ratio': '',
   }
 }
 
